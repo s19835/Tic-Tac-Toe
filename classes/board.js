866a987808f1974c28985f8a9f6114cd4bc9b086
s19835 <s19835@sci.pdn.ac.lk>
2024-06-12 20:39:49 +0530

    create board

---
+++ classes/board.js
@@ -0,0 +1,16 @@
+export default class Board {
+    constructor(state = ["", "", "", "", "", "", "", "", ""]) {
+        this.state = state;
+    }
+    printFormattedBoard() {
+        let formattedString = '';
+        this.state.forEach((cell, index) => {
+            formattedString += cell ? ` ${cell} |` : '   |';
+            if((index + 1) % 3 === 0)  {
+                formattedString = formattedString.slice(0,-1);
+                if(index < 8) formattedString += '\n\u2015\u2015\u2015 \u2015\u2015\u2015 \u2015\u2015\u2015\n';
+            }
+        });
+        console.log('%c' + formattedString, 'color: #c11dd4;font-size:16px');
+    }
+}
---
+++ script.js
@@ -0,0 +1,5 @@
+import Board from "./classes/board.js";
+
+const board = new Board(["X", "", "O", "", "O", "", "", ""]);
+
+board.printFormattedBoard();
