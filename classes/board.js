884c7e726875996918087898fc9ad73f5ceb50ab
s19835 <s19835@sci.pdn.ac.lk>
2024-06-13 14:05:01 +0530

    basic game logic added

--- classes/board.js
+++ classes/board.js
@@ -13,4 +13,34 @@ export default class Board {
         });
         console.log('%c' + formattedString, 'color: #c11dd4;font-size:16px');
     }
+    isEmpty() {
+        return this.state.every(cell => !cell);
+    }
+    isFull() {
+        return this.state.every(cell => cell);
+    }
+    isTerminal() {
+        //senario 1: empty board
+        if (this.isEmpty()) return false;
+
+        //senario 2: vertical win
+        if (this.state[0] === this.state[3] && this.state[3] === this.state[6]) return {'winner' : this.state[0], 'direction': 'v', 'column': 1};
+        if (this.state[1] === this.state[4] && this.state[4] === this.state[7]) return {'winner': this.state[1], 'direction': 'v', 'column': 2};
+        if (this.state[2] === this.state[5] && this.state[5] === this.state[8]) return {'winner': this.state[2], 'direction': 'v', 'column': 3};
+
+        //senario 3: horizontal win
+        if (this.state[0] === this.state[1] && this.state[1] === this.state[2]) return {'winner': this.state[0], 'direction': 'h', 'row': 1};
+        if (this.state[3] === this.state[4] && this.state[4] === this.state[5]) return {'winner': this.state[3], 'direction': 'h', 'row': 2};
+        if (this.state[6] === this.state[7] && this.state[7] === this.state[8]) return {'winner': this.state[6], 'direction': 'h', 'row': 3};
+
+        //senario 4: diagonal win
+        if (this.state[0] === this.state[4] && this.state[4] === this.state[8]) return {'winner' : this.state[0], 'direction': 'd', 'diagonal': true};
+        if (this.state[2] === this.state[4] && this.state[4] === this.state[6]) return {'winner': this.state[2], 'direction': 'd', 'diagonal': true};
+
+        //senario 5: draw
+        if (this.isFull) return {'winner': 'draw'};
+
+        //otherwise
+        return false;
+    }
 }
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 import Board from "./classes/board.js";
 
-const board = new Board(["X", "", "O", "", "O", "", "", ""]);
+const board = new Board(["X", "O", "X", "X", "O", "O", "O", "O", "O"]);
 
 board.printFormattedBoard();
+
+// console.log(board.isEmpty());
+// console.log(board.isFull());
+// console.log(board.isTerminal());
