79e8b67813a1893bde0921d26b15935e5e970069
s19835 <s19835@sci.pdn.ac.lk>
2024-06-14 21:26:51 +0530

    getBestMove method added

--- classes/player.js
+++ classes/player.js
@@ -7,6 +7,10 @@ export default class Player {
         //check if its a new move
         if (depth == 0) this.nodesMap.clear();
 
-        
+        if (board.isTerminal() || depth == this.maxDepth) {
+            if (board.isTerminal().winner === 'X') return 100 - depth;
+            else if (board.isTerminal().winner === 'O') return -100 + depth;
+        }
+        return 0;
     }
 }
