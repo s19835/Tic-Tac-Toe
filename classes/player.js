3eb8ceea758f5b87853fb3cae138dbba5761d945
s19835 <s19835@sci.pdn.ac.lk>
2024-06-15 12:09:27 +0530

    maximinzing half

--- classes/player.js
+++ classes/player.js
@@ -7,10 +7,23 @@ export default class Player {
         //check if its a new move
         if (depth == 0) this.nodesMap.clear();
 
+        //check the terminal state
         if (board.isTerminal() || depth == this.maxDepth) {
             if (board.isTerminal().winner === 'X') return 100 - depth;
             else if (board.isTerminal().winner === 'O') return -100 + depth;
+        } return 0;
+
+        if (maximizing) {
+            let best = -100;
+            board.getAvailableMoves().forEach(index => {
+                //initializing a new board with current state
+                const child = new board([...board.state]);
+                child.insert('X', index);
+
+                //now get the best move from current board
+                const nodeVlaue = this.getBestMove(child, false, callback, depth+1)
+                best = Math.max(best, nodeVlaue);
+            });
         }
-        return 0;
     }
 }
